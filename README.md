178fdc75e96207c1a7167309d6180a6c191afc42
yasi98 <ypakdel@ucdavis.edu>
2024-04-26 10:59:08 -0700

    dataloader added

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Using MNIST Database of Handwritten Digits
 https://archive.ics.uci.edu/dataset/683/mnist+database+of+handwritten+digits
 
 SFSU 871 Project by : Chen-kai Liu, Yasaman Pakdel, Ishit Patel
+
+Download and unzip the datasets from http://yann.lecun.com/exdb/mnist/ into ./dataset folder
+
---
+++ digits.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import os
+
+
+
+def load_mnist_data(data_dir):
+    train_data_path = os.path.join(data_dir, 'train-images.idx3-ubyte')
+    train_labels_path = os.path.join(data_dir, 'train-labels.idx1-ubyte')
+    test_data_path = os.path.join(data_dir, 't10k-images.idx3-ubyte')
+    test_labels_path = os.path.join(data_dir, 't10k-labels.idx1-ubyte')
+
+
+    # Load training images
+    with open(train_data_path, 'rb') as f:
+        magic, num_images, rows, cols = np.fromfile(f, dtype=np.dtype('>i4'), count=4)
+        train_data = np.fromfile(f, dtype=np.uint8).reshape(num_images, rows, cols)
+
+    # Load training labels
+    with open(train_labels_path, 'rb') as f:
+        magic, num_labels = np.fromfile(f, dtype=np.dtype('>i4'), count=2)
+        train_labels = np.fromfile(f, dtype=np.uint8)
+
+    # Load test images
+    with open(test_data_path, 'rb') as f:
+        magic, num_images, rows, cols = np.fromfile(f, dtype=np.dtype('>i4'), count=4)
+        test_data = np.fromfile(f, dtype=np.uint8).reshape(num_images, rows, cols)
+
+    # Load test labels
+    with open(test_labels_path, 'rb') as f:
+        magic, num_labels = np.fromfile(f, dtype=np.dtype('>i4'), count=2)
+        test_labels = np.fromfile(f, dtype=np.uint8)
+
+    return train_data, train_labels, test_data, test_labels
+
+#datasets are already downloaded and unziped in ./dataset 
+dataset_path = './dataset'
+train_images, train_labels, test_images, test_labels = load_mnist_data(dataset_path)
+#print shapes of train_images, train_labels, test_images, test_labels
+print(train_images.shape, train_labels.shape, test_images.shape, test_labels.shape)
