a961f2964d8f221bd502ebdab2aa1f1aeadbef2e
yasi98 <ypakdel@ucdavis.edu>
2024-04-29 14:26:24 -0700

    merged data loader with train.ipynb

--- digits.py
+++ digits.py
@@ -1,9 +1,12 @@
-import numpy as np
-import pandas as pd
+import matplotlib.pyplot as plt
+import pandas as p
 import torch
+import numpy as np
 import torch.nn as nn
 import os
-
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import DataLoader, TensorDataset
 
 
 def load_mnist_data(data_dir):
@@ -33,16 +36,43 @@ def load_mnist_data(data_dir):
         magic, num_labels = np.fromfile(f, dtype=np.dtype('>i4'), count=2)
         test_labels = np.fromfile(f, dtype=np.uint8)
 
+    # Convert images to torch tensors and apply augmentation transforms
+    transform_train = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.RandomCrop(28, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+    
+    # Normalizing the test images
+    transform_test = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,)),
+    ])
+   
+    train_data = torch.stack([transform_train(img) for img in train_data])
+    test_data = torch.stack([transform_test(img) for img in test_data])
+
     return train_data, train_labels, test_data, test_labels
+
 
 def main():
     #datasets are already downloaded and unziped in ./dataset 
     dataset_path = './dataset'
     train_images, train_labels, test_images, test_labels = load_mnist_data(dataset_path)
+
     #print shapes of train_images, train_labels, test_images, test_labels
     print(train_images.shape, train_labels.shape, test_images.shape, test_labels.shape)
 
-    return train_images, train_labels, test_images, test_labels
+    # Create DataLoader objects for images and labels
+    train_images_dataset = TensorDataset(train_images, torch.tensor(train_labels))
+    test_images_dataset = TensorDataset(test_images, torch.tensor(test_labels))
+
+    train_loader = DataLoader(train_images_dataset, batch_size=32, shuffle=True)
+    test_loader = DataLoader(test_images_dataset, batch_size=32, shuffle=False)
+
 
 if __name__ == '__main__':
-    train_images, train_labels, test_images, test_labels = main()
+    main()
